d7f0a00976ae20cd485dbdd35a934f6fa6d487a3
“최수민git config --global user.email “smchoi@gpters.org <guest-account-temp@guest-account-tempui-MacBookPro.local>
2025-10-29 16:30:17 +0900

    Initial commit: 메타몽 과부하 PWA 앱

---
+++ sw.js
@@ -0,0 +1,40 @@
+// Service Worker - PWA 설치를 위한 필수 파일
+const CACHE_NAME = 'metamong-v41';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// 설치
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 활성화
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
